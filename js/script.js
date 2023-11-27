382e144bcd04be387aa18534d68464231b678e69
jkyleJ <78764152+jkyleJ@users.noreply.github.com>
2023-11-27 15:49:14 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+document.getElementById("change-chair").addEventListener("click", function() {
+
+    document.getElementById("modern_chair").src = "./model/modern_chair_1.glb";
+});
+
+document.getElementById("change-classic-chair").addEventListener("click", function() {
+
+    document.getElementById("chair").src = "./model/scan_chair_1.glb";
+});
+
+document.getElementById("change-desk").addEventListener("click", function() {
+
+    document.getElementById("desk").src = "./model/modern_desk2.glb";
+});
